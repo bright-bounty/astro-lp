0dcbe15ce601632d19b9052dca2d87be0952fda6
Tomasz Sójka <tomek@Air-Tomasz.home>
2024-04-27 19:03:16 +0200

    improve readability

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,30 +2,27 @@ function modal() {
   const heroContent = document.querySelector(".hero__content");
   const modalContent = document.querySelector("#modal");
   const checkOfferBtn = document.querySelector(".hero--cta");
-  const confrimModalBtn = modalContent.querySelector(".modal-btn--confirm");
   const closeModalBtn = modalContent.querySelector(".modal-btn--close");
   const closeModalIcon = modalContent.querySelector(".modal-icon--close");
 
   function close() {
     modalContent.classList.remove("modal--show");
     heroContent.classList.remove("blur");
-
     checkOfferBtn.disabled = false;
-
     document.removeEventListener("click", closeOutside, true);
+  }
+
+  function closeOutside(e) {
+    if (!modalContent.contains(e.target)) {
+      close();
+    }
   }
 
   function open() {
     modalContent.classList.add("modal--show");
     heroContent.classList.add("blur");
-
     checkOfferBtn.disabled = true;
-
     document.addEventListener("click", closeOutside, true);
-  }
-
-  function closeOutside(e) {
-    modalContent.contains(e.target) ? "" : close();
   }
 
   checkOfferBtn.addEventListener("click", open);
