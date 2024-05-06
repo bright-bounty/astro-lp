6ab06ab97cbadedb5930233d89be536286959143
Tomasz Sójka <tomek@air-tomasz.home>
2024-05-06 16:24:18 +0200

    do some changes in code

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,18 +1,18 @@
-function burgerNavigation() {
+function burgerList() {
   const mobileIcon = document.getElementById("mobile-navigation");
   const mobileBars = mobileIcon.querySelector(".nav-bars");
-  const navigation = document.getElementById("navigation-list");
-  const allLinks = navigation.querySelectorAll("li");
+  const list = document.getElementById("navigation-list");
+  const allListItem = list.querySelectorAll("li");
 
   function toogle() {
-    navigation.classList.toggle("nav-hover");
+    list.classList.toggle("nav-hover");
     mobileBars.children[0].classList.toggle("rotate-right");
     mobileBars.children[1].classList.toggle("hidden");
     mobileBars.children[2].classList.toggle("rotate-left");
-    document.querySelector("body").classList.toggle("overflow-y-hidden");
+    document.body.classList.toggle("overflow-y-hidden");
   }
 
-  allLinks.forEach((link) => {
+  allListItem.forEach((link) => {
     link.addEventListener("click", toogle);
   });
 
